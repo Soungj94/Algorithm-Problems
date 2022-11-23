a90b353d9cb131a4d710f6b04f475f38765a4417
soungj94 <soungj94@gmail.com>
2022-11-23 11:13:33 +0900

    "항해99 알고리즘 모이고사"

---
+++ 항해99 algorithm problems/모의고사/midterm algorithm.js
@@ -0,0 +1,33 @@
+//1번문제
+// function solution(month, day){
+//     let a= month;
+//     let b= day
+//     const today = new Date(2022,a-1,b);
+
+// const result = today.setDate(today.getDate() + 98);
+// const tMonth = today.getMonth()
+// const dDays = today.getDate()
+// const answer = (tMonth +1 + '월'+ dDays +'일')
+// return answer
+
+// }
+// console.log(solution(1, 18))
+
+
+//2번문제
+function solution(arr1, arr2){
+	let answer=0;
+	return answer;
+}
+let arr1=[9, 9, 9, 9, 7, 9, 8];
+let arr2=[23, 23, 30, 28, 30, 23, 23];
+console.log(solution(arr1, arr2))
+
+//3번문제 
+function solution(s){
+	let answer="";
+	return answer;
+}
+let s="97 75 88 99 95 92 73";
+
+console.log(solution(s))
